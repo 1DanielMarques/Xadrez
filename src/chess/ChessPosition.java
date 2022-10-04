eba0f5113af4232015576df3e062aa9597e56e01
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-04 15:29:43 -0300

    Convertendo as posicoes

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosition {
 
     private char column;
@@ -13,5 +15,20 @@ public class ChessPosition {
         this.row = row;
     }
 
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition() {
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    }
 
 }
