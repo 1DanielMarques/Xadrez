4de29643af5905c26907053bb895a9cd7fb1f885
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-04 14:56:01 -0300

    excecao metodo piece() classe Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -30,6 +30,9 @@ public class Board {
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)){
+            throw new BoardException("Position not on the board!");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
