ace90aa5be70df819eaaa04f84c07edddc043feb
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-04 14:52:52 -0300

    exception no construtor board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -47,7 +50,8 @@ public class Board {
     public boolean positionExists(Position position) {
         return positionExists(position.getRow(), position.getColumn());
     }
-    public boolean thereIsAPiece(Position position){
+
+    public boolean thereIsAPiece(Position position) {
         return piece(position) != null;
     }
 
