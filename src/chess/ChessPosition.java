4b82042525bc55c7b673889f0e862dfa432ad5f1
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-04 15:35:10 -0300

    Colocando pecas padrao posicao xadrez

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -31,4 +31,8 @@ public class ChessPosition {
         return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
     }
 
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
 }
