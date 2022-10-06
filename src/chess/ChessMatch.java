3da1e19c4d1b03d6b1fdb8facd782a28dc245c77
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-06 15:44:45 -0300

    verificacao cor de quem eh a vez de jogar

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -47,8 +47,8 @@ public class ChessMatch {
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
         validateTargetPosition(source, target);
-
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -62,6 +62,9 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
+        }
+        if (currentPlayer != ((ChessPiece) board.piece(position)).getColor()) {
+            throw new ChessException("The chosen piece is not yours");
         }
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece");
