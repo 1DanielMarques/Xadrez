4b16535857eb027ed15aab4e1d4ba9ca280d7dc0
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-10 14:33:07 -0300

    jogada especial en passant BLACK

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -50,8 +50,6 @@ public class Pawn extends ChessPiece {
                     mat[right.getRow() - 1][right.getColumn()] = true;
                 }
             }
-
-
         } else {
             p.setValues(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
@@ -69,6 +67,17 @@ public class Pawn extends ChessPiece {
             p.setValues(position.getRow() + 1, position.getColumn() + 1);
             if (getBoard().positionExists(p) && isThereOppoonentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
+            }
+            // en passant black
+            if (position.getRow() == 4) {
+                Position left = new Position(position.getRow(), position.getColumn() - 1);
+                if (getBoard().positionExists(left) && isThereOppoonentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+                    mat[left.getRow() + 1][left.getColumn()] = true;
+                }
+                Position right = new Position(position.getRow(), position.getColumn() + 1);
+                if (getBoard().positionExists(right) && isThereOppoonentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+                    mat[right.getRow() + 1][right.getColumn()] = true;
+                }
             }
         }
         return mat;
