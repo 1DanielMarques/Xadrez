ff72ad1c6011fe5a90a1143f21a9f5ac2e29148e
mDanielSE <daniel.marques@edu.unifil.br>
2022-10-10 14:10:02 -0300

    tratamento roque pequeno metodo makeMove

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -87,6 +87,15 @@ public class ChessMatch {
             piecesOnTheBoard.remove(capturedPiece);
             capturedPieces.add(capturedPiece);
         }
+        // roque pequeno
+        if (p instanceof King && target.getColumn() == source.getColumn() + 2) {
+            Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+            Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+            ChessPiece rook = (ChessPiece) board.removePiece(sourceT);
+            board.placePiece(rook, targetT);
+            rook.increaseMoveCount();
+        }
+
         return capturedPiece;
     }
 
